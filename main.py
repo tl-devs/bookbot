9096237a19d503ec34316c781666a5669d70b4e7
TL <tl-devs@pm.me>
2025-12-30 17:45:55 +0100

    final checks

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import book_text_word_cnt, book_text_char_cnt, sort_on
+import stats as st
 import sys
 
 def main():
@@ -8,15 +8,17 @@ def main():
         sys.exit(1)
 
     file_path = sys.argv[1]
-    book_len = book_text_word_cnt(file_path)
-    char_cnt = book_text_char_cnt(file_path)#.sort(reverse=True, key=sort_on)
 
-    print(f"""============ BOOKBOT ============
-Analyzing book found at {file_path}...
------------ Word Count ----------
-Found {book_len} total words
---------- Character Count -------
-""", end="")
+    file_to_process = st.get_book_text(file_path)
+
+    book_len = st.book_text_word_cnt(file_to_process)
+    char_cnt = st.book_text_char_cnt(file_to_process)
+
+    print(f"============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print(f"----------- Word Count ----------")
+    print(f"Found {book_len} total words")
+    print(f"--------- Character Count -------")
 
     for key, value in char_cnt.items():
         print(f"{key}: {value}")
--- stats.py
+++ stats.py
@@ -1,16 +1,18 @@
 def get_book_text(file_path):
-    with open(file_path) as f:
-        file_content = f.read()
-
-    return file_content
+    try:
+        with open(file_path) as f:
+            return f.read()
+            
+    except FileNotFoundError:
+        print(f"Error: File at {file_path} not found.")
+        return None
 
 def book_text_word_cnt(input_book):
-    book_to_split = get_book_text(input_book)
-    split_book = book_to_split.split()
+    split_book = input_book.split()
     return len(split_book)
 
 def book_text_char_cnt(input_book):
-    book_to_char = (get_book_text(input_book)).lower()
+    book_to_char = input_book.lower()
     char_uniq_set = set(book_to_char)
     char_dict = dict.fromkeys(char_uniq_set, 0)
 
