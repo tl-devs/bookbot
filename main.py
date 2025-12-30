31a8ad8a1be43d60a5eac07b217a63ec35849e36
TL <tl-devs@pm.me>
2025-12-30 13:47:03 +0100

    function adjustment

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from stats import book_text_word_cnt
+from stats import book_text_word_cnt, book_text_char_cnt
 
 def main():
     test_file_path = "books/frankenstein.txt"
     book_len = book_text_word_cnt(test_file_path)
     print(f"Found {book_len} total words")
+
+    for key, value in book_text_char_cnt(test_file_path).items():
+        print(f"'{key}': {value}")
 
 main()
--- stats.py
+++ stats.py
@@ -8,3 +8,13 @@ def book_text_word_cnt(input_book):
     book_to_split = get_book_text(input_book)
     split_book = book_to_split.split()
     return len(split_book)
+
+def book_text_char_cnt(input_book):
+    book_to_char = (get_book_text(input_book)).lower()
+    char_uniq_set = set(book_to_char)
+    char_dict = dict.fromkeys(char_uniq_set, 0)
+
+    for i in book_to_char:
+        char_dict[i] += 1
+
+    return char_dict
